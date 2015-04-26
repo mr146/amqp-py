90bdae9c59d1b2414ebbb236bc2343c171b65abd
mr146 <den.mukhametianov@gmail.com>
2015-04-27 00:44:34 +0500

    добавил  mystem

--- receiver.py
+++ receiver.py
@@ -2,12 +2,21 @@
 import pika
 import sys
 import time
+from contract_pb2 import Request
+import subprocess
 
 def on_request(ch, method, properties, body):
 	print('got something')
-	print(body)
+	request = Request()
+	request.ParseFromString(body)
+	client_id = request.client_id
+	print(client_id)
+	content = request.content.encode('utf8')
+	echo_pr = subprocess.Popen(['echo', content], stdout = subprocess.PIPE)
+	result_pr = subprocess.Popen(['mystem', '-gnid'], stdin = echo_pr.stdout, stdout = subprocess.PIPE)
+	result = result_pr.communicate()[0]
 	time.sleep(5)
-	channel.basic_publish(exchange = 'response_ex', routing_key = body, body = 'Hi, {0}!'.format(body))
+	channel.basic_publish(exchange = 'response_ex', routing_key = client_id, body = result)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
 channel = connection.channel()
--- sender.py
+++ sender.py
@@ -2,18 +2,27 @@
 import pika
 import uuid
 import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+from contract_pb2 import Request
 
-sid = str(uuid.uuid4())
-print sid
 
 def on_response(ch, methon, properties, body):
 	print(body)
 	channel.close()
 
+request = Request()
+client_id = str(uuid.uuid4())
+request.client_id = client_id
+print(request.client_id)
+filename = sys.argv[1]
+f = open(filename, "r")
+request.content = f.read().decode('utf8')
+
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = 'localhost'))
 channel = connection.channel()
 channel.exchange_declare(exchange = 'request_ex', type = 'direct')
-channel.basic_publish(exchange = 'request_ex', routing_key = '', body = sid)
+channel.basic_publish(exchange = 'request_ex', routing_key = '', body = request.SerializeToString())
 connection.close()
 
 
@@ -23,7 +32,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange = 'response_ex', type = 'direct')
 result = channel.queue_declare(exclusive = True)
 queue_name = result.method.queue
-channel.queue_bind(exchange = 'response_ex', queue = queue_name, routing_key = sid)
+channel.queue_bind(exchange = 'response_ex', queue = queue_name, routing_key = client_id)
 
 channel.basic_consume(on_response, queue = queue_name)
 channel.start_consuming()
